0a0d4cff2ec7b46038a1e9712a27a1427d54d211
4nttiJokela <antti.jokela2@gmail.com>
2026-01-12 10:00:10 +0200

    Initial commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+public class App {
+    public static void main( String[] args ) {
+        Animal animal = new Animal("Pikseli", "Hedgehog", 13);
+
+        System.out.println(animal.name);
+        System.out.println(animal.species);
+        System.out.println(animal.age);
+    }
+}
